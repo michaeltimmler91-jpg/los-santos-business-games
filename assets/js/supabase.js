04c69ad74a3c516c7da1407f95d51c88aca000c1
michaeltimmler91-jpg <michaeltimmler91@googlemail.com>
2026-05-22 00:22:31 +0200

    Create supabase.js

---
+++ assets/js/supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://eulfqqkxqxjgszqdffhy.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_c3bjfIzI3Qz959O6e_GqKg_5XrgbD11";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
