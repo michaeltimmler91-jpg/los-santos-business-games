2c9b7ac9232d721b21ed37fdebab34025d71bd62
michaeltimmler91-jpg <michaeltimmler91@googlemail.com>
2026-05-21 23:11:19 +0200

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,22 +1,33 @@
-const TEAMS = [
+const GAME_CONFIG = {
+    actionEnergyCost:20,
+    minPointsPerAction:5,
+    maxPointsPerAction:20,
+    maxEnergy:100
+};
+
+const DEFAULT_TEAMS = [
     {
         id:"red",
         name:"Red Dragons",
         color:"#ef4444",
-        points:0
+        points:0,
+        leader:null,
+        members:[]
     },
-
     {
         id:"blue",
         name:"Blue Wolves",
         color:"#3b82f6",
-        points:0
+        points:0,
+        leader:null,
+        members:[]
     },
-
     {
         id:"green",
         name:"Green Vipers",
         color:"#22c55e",
-        points:0
+        points:0,
+        leader:null,
+        members:[]
     }
 ];
