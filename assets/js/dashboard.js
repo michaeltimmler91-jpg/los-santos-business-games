49020af65b84722572384f477f1a6c9ff317cf1b
michaeltimmler91-jpg <michaeltimmler91@googlemail.com>
2026-05-21 23:09:46 +0200

    Create dashboard.js

---
+++ assets/js/dashboard.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+    initDashboard();
+});
+
+function initDashboard(){
+    const player = loadData("player");
+
+    if(!player){
+        window.location.href = "../index.html";
+        return;
+    }
+
+    initTeamsIfNeeded();
+    updatePlayerInfo();
+    updateEnergyDisplay();
+    renderRanking();
+
+    document
+    .getElementById("actionBtn")
+    .addEventListener("click", performPlayerAction);
+}
+
+function updatePlayerInfo(){
+    const player = loadData("player");
+    const teams = getTeams();
+    const team = teams.find(t => t.id === player.team);
+
+    document.getElementById("playerInfo").innerText =
+        player.name + " | Team: " + team.name;
+}
+
+function updateEnergyDisplay(){
+    const player = loadData("player");
+
+    document.getElementById("energyText").innerText =
+        player.energy + " / 100";
+
+    document.getElementById("energyFill").style.width =
+        player.energy + "%";
+}
