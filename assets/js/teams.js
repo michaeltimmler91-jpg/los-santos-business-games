4e29f4732595354df52ab9af400418d72fd79b58
michaeltimmler91-jpg <michaeltimmler91@googlemail.com>
2026-05-21 23:05:32 +0200

    Update teams.js

--- assets/js/teams.js
+++ assets/js/teams.js
@@ -1,1 +1,39 @@
+let selectedTeam = null;
 
+function renderTeams(){
+
+    const teamList =
+    document.getElementById("teamList");
+
+    teamList.innerHTML = "";
+
+    TEAMS.forEach(team => {
+
+        const div =
+        document.createElement("div");
+
+        div.className = "team-card";
+
+        div.style.background = team.color;
+
+        div.innerHTML = `
+            <h3>${team.name}</h3>
+            <p>Punkte: ${team.points}</p>
+        `;
+
+        div.addEventListener("click",()=>{
+
+            document
+            .querySelectorAll(".team-card")
+            .forEach(card => {
+                card.classList.remove("active");
+            });
+
+            div.classList.add("active");
+
+            selectedTeam = team.id;
+        });
+
+        teamList.appendChild(div);
+    });
+}
