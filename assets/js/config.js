d4c3f816b101a202b0fe036a3f02f2b9df49fa9a
michaeltimmler91-jpg <michaeltimmler91@googlemail.com>
2026-05-21 23:05:10 +0200

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,1 +1,22 @@
+const TEAMS = [
+    {
+        id:"red",
+        name:"Red Dragons",
+        color:"#ef4444",
+        points:0
+    },
 
+    {
+        id:"blue",
+        name:"Blue Wolves",
+        color:"#3b82f6",
+        points:0
+    },
+
+    {
+        id:"green",
+        name:"Green Vipers",
+        color:"#22c55e",
+        points:0
+    }
+];
