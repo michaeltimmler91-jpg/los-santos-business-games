16507cf383e45c052806374cc9e96c76de08818c
michaeltimmler91-jpg <michaeltimmler91@googlemail.com>
2026-05-21 23:05:44 +0200

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,1 +1,29 @@
+function createPlayer(){
 
+    const playerName =
+    document
+    .getElementById("playerName")
+    .value
+    .trim();
+
+    if(!playerName){
+        alert("Bitte Spielernamen eingeben");
+        return;
+    }
+
+    if(!selectedTeam){
+        alert("Bitte ein Team wählen");
+        return;
+    }
+
+    const player = {
+        name:playerName,
+        team:selectedTeam,
+        energy:100
+    };
+
+    saveData("player",player);
+
+    window.location.href =
+    "pages/dashboard.html";
+}
